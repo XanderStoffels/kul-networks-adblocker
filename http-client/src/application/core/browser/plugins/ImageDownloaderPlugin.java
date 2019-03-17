2aea99ba330148efdb11df7fbd9d3734160ed182
Xander <xander.s@outlook.com>
2019-03-17 19:01:47 +0100

    Push 2 for laptop

--- http-client/src/application/core/browser/plugins/ImageDownloaderPlugin.java
+++ http-client/src/application/core/browser/plugins/ImageDownloaderPlugin.java
@@ -1,14 +1,65 @@
-package application.core.browser.plugins.api;
+package application.core.browser.plugins;
 
+import application.core.browser.plugins.api.IWebBrowserPlugin;
 import application.core.client.api.IHttpClient;
+import application.exceptions.HttpClientException;
+import messaging.api.IHttpRequest;
 import messaging.api.IHttpResponse;
+import messaging.imp.HttpRequest;
+import messaging.model.HttpMethod;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class ImageDownloaderPlugin implements IWebBrowserPlugin {
     @Override
-    public IHttpResponse passThrough(IHttpClient client, IHttpResponse response) {
-        
+    public IHttpResponse passThrough(IHttpClient client,  IHttpRequest originalRequest, IHttpResponse originalResponse) {
+        String htmlString = new String(originalResponse.getBody());
+        Document doc = Jsoup.parse(htmlString);
+        Elements images = doc.getElementsByTag("img");
 
-
-
+        images.forEach(image -> makeImageRequest(client, originalRequest, image));
+        return originalResponse;
     }
+
+    private void makeImageRequest(IHttpClient client, IHttpRequest originalRequest, Element image) {
+        String source = image.attr("src");
+
+        IHttpRequest imageRequest = new HttpRequest(HttpMethod.GET);
+        imageRequest.setUrlTail("/" + source);
+
+        imageRequest.getHeaders().set("Host", originalRequest.getHeaders().getOrEmpty("Host"));
+        imageRequest.getHeaders().set("Connection", originalRequest.getHeaders().getOrEmpty("Connection"));
+
+        try {
+            IHttpResponse imageResponse = client.request(imageRequest);
+
+            Thread.sleep(500);
+            File outputDirectory = Paths.get("downloads", client.getBaseUrl()).toFile();
+            outputDirectory.mkdirs();
+
+            File outputFile = Paths.get(
+                    "downloads",
+                    client.getBaseUrl(),
+                    source)
+                    .toFile();
+
+            FileOutputStream imageSaveSteam = new FileOutputStream(outputFile);
+            imageSaveSteam.write(imageResponse.getBody());
+            imageSaveSteam.flush();
+            imageSaveSteam.close();
+
+        } catch (HttpClientException | IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- http-client/src/application/core/browser/plugins/api/IWebBrowserPlugin.java
+++ http-client/src/application/core/browser/plugins/api/IWebBrowserPlugin.java
@@ -1,4 +1,9 @@
 package application.core.browser.plugins.api;
 
+import application.core.client.api.IHttpClient;
+import messaging.api.IHttpRequest;
+import messaging.api.IHttpResponse;
+
 public interface IWebBrowserPlugin {
+    IHttpResponse passThrough(IHttpClient client, IHttpRequest originalRequest, IHttpResponse originalResponse);
 }
